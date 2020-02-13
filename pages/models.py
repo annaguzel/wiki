e3035bbde10edaf5c110006bcc0ada26b69d41a9
Annaguzel <you@example.com>
2020-02-13 13:52:33 +0200

    Wiki

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+
+class Page(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    last_update=models.DateTimeField(auto_now=True)
+
+
+    def get_absolute_url(self):
+
+        return reverse("page-detail",args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
+
+
 
 # Create your models here.
--- wiki/urls.py
+++ wiki/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/',views.welcome ,name='hello-world'),
+    path('pages/list/', views.page_list ,name="page-list"),
+    path('pages/detail/<int:page_id>/', views.page_detail ,name="page-detail"),
+
+
+
+
 ]
